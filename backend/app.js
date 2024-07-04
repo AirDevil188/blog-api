1b946f93e3d0ed094641371696e872cdbc1f7f5b
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-07-04 11:51:31 +0200

    using middleware routes

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,15 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const dotEnv = require("dotenv");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
+const userRouter = require("./routes/users");
 
 const app = express();
-dotEnv.config();
 const mongoDB = process.env.CONNECTION_STRING;
 
 mongoose.connect(mongoDB);
@@ -23,12 +25,13 @@ app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
+app.use("/users", userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
