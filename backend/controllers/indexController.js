e2c5645183c655ffb48165ba2f43bcf027701f96
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-07-04 11:48:58 +0200

    added indexController

---
+++ backend/controllers/indexController.js
@@ -0,0 +1,20 @@
+// const mongoose = require("mongoose");
+// const asyncHandler = require("express-async-handler");
+// const Post = require("../models/post");
+
+// exports.blog_index_controller = asyncHandler(async (req, res, next) => {
+//   const post = await Post.find().sort({ title: 1 }).exec();
+
+//   return res.send(Object.values(post));
+// });
+
+const mongoose = require("mongoose");
+const asyncHandler = require("express-async-handler");
+
+const Post = require("../models/post");
+
+exports.blog_index_controller = asyncHandler(async (req, res, next) => {
+  const post = await Post.find().sort({ title: 1 }).exec();
+
+  res.send(Object.values(post));
+});
