29078072fb6a6793dcc1ebc6816b5c833beff705
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-07-08 12:22:26 +0200

    fixed expiration-time for jwt token

--- backend/config/jwtCreateToken.js
+++ backend/config/jwtCreateToken.js
@@ -1,11 +1,10 @@
 const jwt = require("jsonwebtoken");
 
 exports.jwtCreateToken = (req, res) => {
-  const expirationTime = Math.floor(Date.now() / 1000) + 25 * 60; // 25 minutes
   jwt.sign(
     { user: req.user._id },
     process.env.SECRET,
-    { expiresIn: expirationTime },
+    { expiresIn: "25min" },
     (err, token) => {
       res.json({
         token,
