6441585bc0827e37482a1c2cbe1483ef1149899d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-07-10 12:38:41 +0200

    refactored postController

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,21 +1,19 @@
-const express = require("express");
 const asyncHandler = require("express-async-handler");
+const validator = require("express-validator");
 
 const Post = require("../models/post");
 
-const mongoose = require("mongoose");
-
-exports.posts_get_controller = asyncHandler(async (req, res, next) => {
+exports.list_posts_controller_get = asyncHandler(async (req, res, next) => {
   const post = await Post.find().sort({ title: 1 }).exec();
 
   res.send(Object.values(post));
 });
 
-exports.post_create_controller = asyncHandler(async (req, res, next) => {
+exports.post_create_controller_post = asyncHandler(async (req, res, next) => {
   return res.send("Creating Post");
 });
 
-exports.post_update_controller = asyncHandler(async (req, res, next) => {
+exports.post_update_controller_put = asyncHandler(async (req, res, next) => {
   return res.send("Updating Post");
 });
 
