909c03b798fb6dfd0d4a50aafe26fd74ae4a47b8
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-07-12 17:28:35 +0200

    added Sign-up component

---
+++ frontend/src/components/Sign-up.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const SignUp = () => {
+  const [error, setError] = useState(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const options = {
+      method: "POST",
+      body: new URLSearchParams(formData),
+    };
+    try {
+      const response = await fetch("http://localhost:8080/sign-up", options);
+      if (response.ok) {
+        response.json();
+      } else {
+        throw {
+          json: setError(await response.json()),
+        };
+      }
+    } catch (err) {
+      return err;
+    }
+  };
+  return (
+    <>
+      <div>
+        <form method="post" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username: </label>
+            <input type="text" id="username" name="username" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password: </label>
+            <input type="password" id="password" name="password" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="confirm_password">Confirm Password: </label>
+            <input
+              type="password"
+              id="confirm_password"
+              name="confirm_password"
+            />
+          </div>
+          <button type="submit">Create Account</button>
+        </form>
+
+        <section>
+          {error &&
+            error[0].errors.map((err, index) => {
+              {
+                return <div key={`${err}-${index}`}>{err.msg}</div>;
+              }
+            })}
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
