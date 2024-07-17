6a42b656824b462adca3de7e3e51cd6bab905b71
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-07-17 12:26:01 +0200

    added LogIn component

---
+++ frontend/src/components/Log-in.jsx
@@ -0,0 +1,72 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../App";
+import { useNavigate } from "react-router-dom";
+
+const LogIn = () => {
+  const [error, setError] = useState(null);
+  const { userObject, setUserObject } = useContext(UserContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const options = {
+      method: "POST",
+      body: new URLSearchParams(formData),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
+    try {
+      const response = await fetch(`http://localhost:8080/log-in`, options);
+      console.log(response, "response");
+      if (response.ok) {
+        const newUser = {
+          ...userObject,
+          username: formData.get("username"),
+          password: formData.get("password"),
+        };
+        setUserObject(newUser);
+        const token = await response.json();
+        localStorage.setItem("token", String(Object.values(token)));
+        setError(null);
+        useNavigate("/");
+      } else {
+        throw {
+          json: setError(await response.json()),
+        };
+      }
+    } catch (err) {
+      return err;
+    }
+  };
+
+  return (
+    <>
+      <div className="main-wrapper">
+        <section className="log-in-section">
+          <form method="POST" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="username">Username: </label>
+              <input type="text" id="username" name="username" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password: </label>
+              <input type="password" name="password" id="password" />
+            </div>
+            <button>LOG IN</button>
+          </form>
+        </section>
+        <section>
+          {error &&
+            error[0].errors.map((err, index) => {
+              {
+                return <div key={`${err}-${index}`}>{err.msg}</div>;
+              }
+            })}
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default LogIn;
