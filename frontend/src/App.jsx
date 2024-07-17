4834cb39f354d937338fd9891ce4f720fcf1f14d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-07-17 12:40:23 +0200

    added checkValidation async function that will check if token is present if it is the user will be logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,43 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/Navbar";
+import { jwtDecode } from "jwt-decode";
+import { DateTime } from "luxon";
 
 export const UserContext = createContext(null);
 
 const App = () => {
-  const [userObject, setUserObject] = useState({
-    username: "",
-    password: "",
-  });
-  console.log(userObject);
+  const [userObject, setUserObject] = useState(null);
+
+  useEffect(() => {
+    const checkValidation = async () => {
+      const token = localStorage.getItem("token");
+      const options = {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${String(localStorage.getItem("token"))}`,
+        },
+      };
+      if (token) {
+        const decoded = jwtDecode(localStorage.getItem("token"));
+        const tokenExpirationDate = DateTime.fromSeconds(decoded.exp);
+        const currentDate = DateTime.fromMillis(Date.now());
+
+        if (currentDate > tokenExpirationDate) {
+          console.log("token expired");
+          localStorage.clear();
+          return;
+        }
+
+        const response = await fetch("http://localhost:8080/", options);
+        setUserObject(response);
+
+        return await response.json();
+      }
+    };
+    checkValidation();
+  }, []);
 
   return (
     <>
